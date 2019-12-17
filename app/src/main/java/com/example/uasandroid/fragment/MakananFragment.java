8af79a533ce70611702ca97fdc235f52634eba9f
Agus Subag <456jojon>
2019-12-17 22:42:06 +0700

    Add dan edit MakananFragment dan fargment_makanan.xml

---
+++ app/src/main/java/com/example/uasandroid/fragment/MakananFragment.java
@@ -0,0 +1,32 @@
+package com.example.uasandroid.fragment;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.uasandroid.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MakananFragment extends Fragment {
+
+
+    public MakananFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_makanan, container, false);
+    }
+
+}
