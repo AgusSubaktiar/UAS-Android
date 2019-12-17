bfd38f072aa484db1f1ee54ad2c45a04a265a720
Agus Subag <456jojon>
2019-12-17 23:55:11 +0700

    Update MainActivity

--- app/src/main/java/com/example/uasandroid/activities/MainActivity.java
+++ app/src/main/java/com/example/uasandroid/activities/MainActivity.java
@@ -1,16 +1,49 @@
 package com.example.uasandroid.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.uasandroid.R;
+import com.example.uasandroid.fragment.CoffeFragment;
+import com.example.uasandroid.fragment.MakananFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.action_food:
+                fragment = new CoffeFragment();
+                break;
+            case R.id.action_movie:
+                fragment = new MakananFragment();
+                break;
+        }
+        return loadFragment(fragment);
     }
 }
+
